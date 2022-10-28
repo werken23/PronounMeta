3cb8b81ea01361ccd5aa4c23eef1b52053eb76a9
werken23 <werken23@icloud.com>
2022-10-28 21:40:28 +0200

    Initial commit

---
+++ src/main/java/nl/engeserver/pronounmeta/Main.java
@@ -0,0 +1,11 @@
+package nl.engeserver.pronounmeta;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("test");
+    }
+}
